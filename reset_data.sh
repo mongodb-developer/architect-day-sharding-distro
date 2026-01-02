b6202cc87ec1183b1a43cf56ef0cff5f78f818a9
Graeme Robinson <graeme.robinson@ymail.com>
2026-01-02 16:35:42 -0700

    modified scripts to handle data reset on EC2 server

--- reset_data.sh
+++ reset_data.sh
@@ -1,11 +1,17 @@
-mongosh --uri="mongodb+srv://perfUser:nQwvQxHSayyyf6nF@architectdaysharded.azzy6.mongodb.net/architect_day?authSource=admin" --eval "db.dropDatabase()"
-mongosh --uri="mongodb+srv://perfUser:nQwvQxHSayyyf6nF@architectdaysharded.azzy6.mongodb.net/architect_day?authSource=admin" --file=createAndShardCols.js
+#!/bin/bash
+
+# Reset the data in the database
+export mongo_uri="mongodb+srv://perfUser:nQwvQxHSayyyf6nF@architectdaysharded.azzy6.mongodb.net/architect_day?authSource=admin"
+mongosh --uri=$mongo_uri --eval "db.dropDatabase()"
+mongosh --uri=$mongo_uri --file=createAndShardCols.js
+
+# Import the data into the database
 if [ -f "orders.json.gz" ]; then
   gunzip -f orders.json.gz
 fi
 if [ -f "orders.json" ]; then
-  mongoimport --uri="mongodb+srv://perfUser:nQwvQxHSayyyf6nF@architectdaysharded.azzy6.mongodb.net/architect_day?authSource=admin" --collection=orders --file=orders.json
-  mongoimport --uri="mongodb+srv://perfUser:nQwvQxHSayyyf6nF@architectdaysharded.azzy6.mongodb.net/architect_day?authSource=admin" --collection=orders_ci --file=orders.json
-  mongoimport --uri="mongodb+srv://perfUser:nQwvQxHSayyyf6nF@architectdaysharded.azzy6.mongodb.net/architect_day?authSource=admin" --collection=orders_dc --file=orders.json
-  mongoimport --uri="mongodb+srv://perfUser:nQwvQxHSayyyf6nF@architectdaysharded.azzy6.mongodb.net/architect_day?authSource=admin" --collection=orders_dch --file=orders.json
+  mongoimport --uri=$mongo_uri --collection=orders --file=orders.json
+  mongoimport --uri=$mongo_uri --collection=orders_ci --file=orders.json
+  mongoimport --uri=$mongo_uri --collection=orders_dc --file=orders.json
+  mongoimport --uri=$mongo_uri --collection=orders_dch --file=orders.json
 fi
