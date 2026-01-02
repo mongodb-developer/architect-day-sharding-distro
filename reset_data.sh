ce13397ee02f547ed8a26856f40274c2c1bfcf36
Graeme Robinson <graeme.robinson@ymail.com>
2026-01-02 16:49:31 -0700

    modified scripts to handle data reset on EC2 server

--- reset_data.sh
+++ reset_data.sh
@@ -1,17 +1,17 @@
 #!/bin/bash
 
 # Reset the data in the database
-export mongo_uri="mongodb+srv://perfUser:nQwvQxHSayyyf6nF@architectdaysharded.azzy6.mongodb.net/architect_day?authSource=admin"
-mongosh --uri=$mongo_uri --eval "db.dropDatabase()"
-mongosh --uri=$mongo_uri --file=createAndShardCols.js
+export MONGO_URI="mongodb+srv://perfUser:nQwvQxHSayyyf6nF@architectdaysharded.azzy6.mongodb.net/architect_day?authSource=admin"
+mongosh $MONGO_URI --eval "db.dropDatabase()"
+mongosh $MONGO_URI --file=createAndShardCols.js
 
 # Import the data into the database
 if [ -f "orders.json.gz" ]; then
   gunzip -f orders.json.gz
 fi
 if [ -f "orders.json" ]; then
-  mongoimport --uri=$mongo_uri --collection=orders --file=orders.json
-  mongoimport --uri=$mongo_uri --collection=orders_ci --file=orders.json
-  mongoimport --uri=$mongo_uri --collection=orders_dc --file=orders.json
-  mongoimport --uri=$mongo_uri --collection=orders_dch --file=orders.json
+  mongoimport --uri=$MONGO_URI --collection=orders --file=orders.json
+  mongoimport --uri=$MONGO_URI --collection=orders_ci --file=orders.json
+  mongoimport --uri=$MONGO_URI --collection=orders_dc --file=orders.json
+  mongoimport --uri=$MONGO_URI --collection=orders_dch --file=orders.json
 fi
